5078ec3e72dd6f3ca60f6ae63efab2d246a79720
acjw <saoft@foxmail.com>
2016-06-14 16:33:57 +0800

    添加web配置

---
+++ hspider-admin-contrl/src/main/java/org/honshe/hspider/config/WebAppConfig.java
@@ -0,0 +1,23 @@
+package org.honshe.hspider.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by honshe on 2016/6/14.
+ */
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+}
--- hspider-admin/src/main/webapp/WEB-INF/views/index.jsp
+++ hspider-admin/src/main/webapp/WEB-INF/views/index.jsp
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <jsp:include page="global/global.jsp"/>
 </head>
 <body>
 <h2>Hello World!</h2>
+<jsp:include page="global/global-script.jsp"/>
 </body>
 </html>
